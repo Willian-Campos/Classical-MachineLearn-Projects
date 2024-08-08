3aa08ade04fd37c166f8880e5227a3ea9d62fd8a
Willian-Campos <willian.augusto.campos@gmail.com>
2024-08-08 15:24:55 -0300

    Atualização do app.py e inclusão do mod38

--- ebac_ciencia-de-dados_projeto-final_streamlit-e-pipeline/app.py
+++ ebac_ciencia-de-dados_projeto-final_streamlit-e-pipeline/app.py
@@ -4,6 +4,7 @@ import joblib
 from pycaret.classification import load_model, predict_model
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import FunctionTransformer, MinMaxScaler
+from sklearn.metrics import confusion_matrix
 
 # Carregar o modelo salvo
 model = load_model('tuned_rf_model')
@@ -17,11 +18,12 @@ quantitativas = ['idade', 'tempo_emprego', 'renda']
 def remover_colunas(df):
     return df.drop(columns=['index', 'data_ref'])
 
-def balanceamento(df):
-    df_1 = df[df['mau'] == 1]
-    n = df_1.shape[0] * 3
-    df_0 = df[df['mau'] == 0].sample(n=n, random_state=0)
-    return pd.concat([df_1, df_0]).sample(frac=1, random_state=0).reset_index(drop=True)
+# ESSA PARTE SERVE APENAS PARA TREINAR O MODELO - deixado aqui para não esquecer que ela existe no notebook .ipynb
+#def balanceamento(df):
+#    df_1 = df[df['mau'] == 1]
+#    n = df_1.shape[0] * 3
+#    df_0 = df[df['mau'] == 0].sample(n=n, random_state=0)
+#    return pd.concat([df_1, df_0]).sample(frac=1, random_state=0).reset_index(drop=True)
 
 def preencher_missings(df):
     df['tempo_emprego'].fillna(df['tempo_emprego'].mean(), inplace=True)
@@ -57,7 +59,7 @@ def min_max_scaler(df):
 
 pipeline = Pipeline(steps=[
     ('remover_colunas', FunctionTransformer(remover_colunas)), 
-    ('balanceamento', FunctionTransformer(balanceamento)), 
+    #('balanceamento', FunctionTransformer(balanceamento)), 
     ('preencher_missings', FunctionTransformer(preencher_missings)),
     ('remove_outliers_renda', FunctionTransformer(remove_outliers_renda)),
     ('agrupar_categorias', FunctionTransformer(agrupar_categorias)),
@@ -71,16 +73,35 @@ def fazer_previsao(dados):
     previsoes = predict_model(model, data=dados_preprocessados)
     return previsoes
 
+# Função para calcular lucro
+def calcular_lucro_total(cm, ganho_TN, perda_FP, perda_FN):
+    # Extraindo TN, FP, FN da matriz de confusão
+    TN = cm[0, 0]
+    FP = cm[0, 1]
+    FN = cm[1, 0]
+    
+    # Calcular lucro total
+    lucro = (TN * ganho_TN) - (FN * perda_FN)
+    lucro_perdido = FP * perda_FP
+    return lucro, lucro_perdido
+
 # Configurar a interface do Streamlit
+st.set_page_config(page_title="CreditScore")
+
 st.title("Aplicativo de Previsão de Crédito")
 st.write("Carregue um arquivo para fazer previsões")
+
+# Campos de entrada para ganho e perda
+ganho_TN = st.number_input("Ganho por True Negative (Adimplentes)", value=1, step=1)
+perda_FP = st.number_input("Perda por False Positive (Adimplentes classificados como inadimplentes)", value=1, step=1)
+perda_FN = st.number_input("Perda por False Negative (Inadimplentes classificados como adimplentes)", value=10, step=1)
 
 # Carregar arquivo
 arquivo_carregado = st.file_uploader("Escolha um arquivo", type=["csv", "xlsx", "ftr"])
 
 if arquivo_carregado is not None:
-    # Identificar o tipo de arquivo e carregar os dados
     try:
+        # Identificar o tipo de arquivo e carregar os dados
         if arquivo_carregado.name.endswith('.csv'):
             dados = pd.read_csv(arquivo_carregado)
         elif arquivo_carregado.name.endswith('.xlsx'):
@@ -101,5 +122,40 @@ if arquivo_carregado is not None:
         st.write("Previsões:")
         st.write(previsoes)
 
+        # Extraindo valores reais e previstos
+        y_true = previsoes['mau']  # Alvo real
+        y_pred = previsoes['prediction_label']  # Predições feitas pelo modelo
+
+        # Calcular matriz de confusão
+        cm = confusion_matrix(y_true, y_pred)
+
+        # Calcular lucro total e lucro perdido
+        lucro, lucro_perdido = calcular_lucro_total(cm, ganho_TN, perda_FP, perda_FN)
+
+        # Mostrar lucro total e lucro perdido
+        st.write(f"Lucro total: {lucro}")
+        st.write(f"Lucro perdido (devido a FalsePositive): {lucro_perdido}")
+
     except Exception as e:
         st.error(f"Erro ao ler o arquivo: {e}")
+
+# Rodapé com link para o GitHub
+st.markdown("""
+    <style>
+        footer {visibility: hidden;}
+        .stApp { bottom: 50px; }
+        .footer {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            background-color: white;
+            text-align: center;
+            padding: 10px;
+            font-size: small;
+        }
+    </style>
+    <div class="footer">
+        <p>Para mais informações sobre o modelo, visite o <a href="https://github.com/Willian-Campos/DataScienceProjects/tree/master/ebac_ciencia-de-dados_projeto-final_streamlit-e-pipeline" target="_blank">repositório no GitHub</a>.</p>
+    </div>
+    """, unsafe_allow_html=True)
